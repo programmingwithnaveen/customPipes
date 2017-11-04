e3c1d974ddbbc51a8d44ec35d3e4f5afa0c9bb35
Naveen Kn <naveen.kn@ros.com>
2017-11-04 11:40:58 -0700

    reformmting applied

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,40 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-     <h1 class="display-3">Custom Pipes Demo</h1>
-  </div>
-</div>
-<table class="table table-bordered table-striped">
-<thead>
-   <tr>
-      <th>Cusomt Pipe Type</th>
-      <th>Example</th>
-   </tr>
-</thead>
-<tbody>
-   <tr>
-      <td>Cusom UpperCase Pipe</td>
-      <td> {{upperCaseText | customUpperCase}}<br></td>
-   </tr>
-   <tr>
-      <td>Custom LowerCase Pipe</td>
-      <td> {{loweCaseText | customLowerCase}}<br></td>
-   </tr>
-   <tr>
-      <td>SummaryCase Pipe</td>
-      <td>   {{summaryText |SummaryPipe}}<br></td>
-   </tr>
-   </tbody>
-   </table>
-          `
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-3">Custom Pipes Demo</h1>
+      </div>
+    </div>
+    <table class="table table-bordered table-striped">
+      <thead>
+      <tr>
+        <th>Cusomt Pipe Type</th>
+        <th>Example</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Cusom UpperCase Pipe</td>
+        <td> {{upperCaseText | customUpperCase}}<br></td>
+      </tr>
+      <tr>
+        <td>Custom LowerCase Pipe</td>
+        <td> {{loweCaseText | customLowerCase}}<br></td>
+      </tr>
+      <tr>
+        <td>SummaryCase Pipe</td>
+        <td> {{summaryText | SummaryPipe}}<br></td>
+      </tr>
+      </tbody>
+    </table>
+  `
 
 })
 export class AppComponent {
-  upperCaseText ='LoReM iPsUm';
-  loweCaseText='LoReM iPsUm';
+  upperCaseText = 'LoReM iPsUm';
+  loweCaseText = 'LoReM iPsUm';
   summaryText = 'Lorem ipsum dolor sit amet, munere consequat ut mei, case verear ius et. Mea cu debet iudico discere, sit ea assum dissentiet. Usu ei nulla homero insolens, ex mel esse quot abhorreant. No modus erant euismod his.';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { customUpperCase, customLowerCase,SummaryPipe} from './custom.pipe';
+import {AppComponent} from './app.component';
+import {customLowerCase, customUpperCase, SummaryPipe} from './custom.pipe';
 
 @NgModule({
   declarations: [
@@ -17,4 +17,5 @@ import { customUpperCase, customLowerCase,SummaryPipe} from './custom.pipe';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
